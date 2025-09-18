b50c19e15ad0df60da218710389cbb8ee0017d20
Moueez-Aleem <encyphersdev1@gmail.com>
2025-09-18 17:24:15 +0500

    Made the chatbot responsive

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from openai import OpenAI
 from flask import Flask, render_template, request, url_for, session, redirect, flash, get_flashed_messages
-from datetime import datetime
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 
 # Load environment variables
-# load_dotenv()
+load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 client = OpenAI(api_key=API_KEY)
@@ -20,7 +19,6 @@ faq_data = """
 
 
 def chatbot_response(user_input):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     prompt = f"""
     You are a friendly AI assistant chatbot.
     You can answer general questions, chat casually, and provide helpful answers.
@@ -29,8 +27,6 @@ def chatbot_response(user_input):
     2. Answer factually when possible.
     3. If you don’t know the answer, say you don’t know.
     4. Do not provide harmful, unsafe, or offensive responses.
-    5. You can mention the current local time if asked.
-    The current time is: {current_time}
     Also you will provide the FAQ data below if the question is related to it:
 
     {faq_data}
@@ -94,12 +90,6 @@ def home():
     # For GET requests
     # chat_open = request.args.get("chat_open") == "1"
     return render_template("index.html", chat_history=session["chat_history"], chat_open=chat_open)
-
-
-# @app.route("/clear")
-# def clear():
-#     session["chat_history"] = []
-#     return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
